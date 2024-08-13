147556b62bfc44e7d53e2775035cb8d45d21e456
Abdallah Khalfan <108978947+heisabdallah@users.noreply.github.com>
2024-08-13 19:43:49 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,29 @@
 import './globals.css'
 import { Oswald, Space_Grotesk } from 'next/font/google'
 import Header from './components/header/header'
-// import Footer from './components/Footer/footer'
 import './scroll.css'
-
+import Script from 'next/script'
 
 const oswald = Oswald({ weight: ["200", "300", "400", "500", "600", "700"], subsets: ['latin'] })
 const space_grotesk = Space_Grotesk({ weight: ["300", "400", "500", "600", "700"], subsets: ['latin'] })
-// const poppins = Poppins({ weight: ["300", "400", "500", "600", "700", "800", "900"], subsets: ['latin'] })
-
 
 export default function RootLayout({ children }) {
-
   return (
-    <html lang="en" style={{scrollBehavior: 'smooth'}} className='scroll-container' >
-      <body className={`bg-cover bg-bottom font-space_grotesk text-base font-light text-slate-700`} >
-      {/* <body className={`bg-[url('/images/backgrounds/bg1.jpg')] bg-cover bg-bottom font-poppins text-gray-700 ${poppins.variable}`} > */}
+    <html lang="en" style={{ scrollBehavior: 'smooth' }} className='scroll-container'>
+      
+      {/* Tawk.to Script */}
+      <Script
+        strategy="lazyOnload"
+        src="https://embed.tawk.to/66bb89580cca4f8a7a757f41/1i56910ni"
+        crossOrigin="anonymous"
+        async
+      />
+
+      <body className={`bg-cover bg-bottom font-space_grotesk text-base font-light text-slate-700`}>
         <header>
           <Header />
         </header>
         <main className='bg-gray-50 bg-opacity-95'>{children}</main>
-        
       </body>
     </html>
   )
